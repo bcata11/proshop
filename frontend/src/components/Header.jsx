cb9d688647f342002b7302b1c239fa110459883f
bcata11 <catalinbar73@gmail.com>
2025-01-03 10:30:41 +0200

    changed logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,8 +37,8 @@ const Header = () => {
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
-              ProShop
+              <img src={logo} alt='SpruceTeam' />
+              SpruceTeam
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
